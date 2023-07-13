f5411f374737c20a1424fd066774f46d716328b0
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-07-12 20:53:50 -0700

    search working, links working

--- src/App.js
+++ src/App.js
@@ -1,42 +1,125 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
-const App = () => {
-  let [search, setSearch] = useState('')
-  let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+function App() {
+    let [search, setSearch] = useState('')
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState([])
 
-  const API_URL = 'https://itunes.apple.com/search?term='
+    const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-      if(search) {
-          const fetchData = async () => {
-              document.title = `${search} music`
-              const response = await fetch(API_URL + search)
-              const resData = await response.json()
-              if (resData.results.length > 0) {
-                  return setData(resData.results)
-              } else {
-                  return setMessage('Not Found.')
-              }
-          }
-          fetchData()
-      }
-  }, [search])
+    useEffect(() => {
+        if(search) {
+            const fetchData = async () => {
+                document.title = `${search} Music`
+                const response = await fetch(API_URL + search)
+                const resData = await response.json()
+                if (resData.results.length > 0) {
+                    return setData(resData.results)
+                } else {
+                    return setMessage('Not Found')
+                }
+            }
+            fetchData()
+        }
+    }, [search])
+    
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        setSearch(term)
+    }
 
-  const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
-  }
-console.log('DATA FROM API!!!', data)
-  return (
-      <div>
-          <SearchBar handleSearch={handleSearch} />
-          {message}
-          <Gallery data={data} />
-      </div>
-  )
+    return (
+        <div>
+        {message}
+            <Router>
+                <Routes>
+                    <Route path="/" element={
+                        <div>
+                            <SearchBar handleSearch = {handleSearch}/>
+                            <Gallery data={data} />
+                        </div>
+                    } />
+                    <Route path="/album/:id" element={<AlbumView />} />
+                    <Route path="/artist/:id" element={<ArtistView />} />
+                </Routes>
+            </Router>
+        </div>
+    )
+    
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+//import { useState, useRef } from 'react'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Gallery from './components/Gallery'
+// import SearchBar from './components/SearchBar'
+// import { DataContext } from './context/DataContext'
+// // import { SearchContext } from './context/SearchContext'
+// import AlbumView from './components/AlbumView'
+// import ArtistView from './components/ArtistView'
+
+
+// function App() {
+// //  let [search, setSearch] = useState
+//  let [message, setMessage] = useState('Search for Music!')
+//  let [data, setData] = useState([])
+//  let searchInput = useRef('')
+
+//     const API_URL = 'https://itunes.apple.com/search?term='
+    
+//     const handleSearch = (e, term) => {
+//         e.preventDefault()
+//         // Fetch Data
+//         const fetchData = async () => {
+//             document.title = `${term} Music`
+//             const response = await fetch(API_URL + term)
+//             const resData = await response.json()
+//             if (resData.results.length > 0) {
+//                 // Set State and Context value
+//                 return setData(resData.results)
+//             } else {
+//                 return setMessage('Not Found')
+//             }
+//         }
+//         fetchData()
+//     }
+    
+//     return (
+//       <div className="App">
+//         {message}
+//         <DataContext />
+//         <Router>
+//           <Routes>
+//             <Route path="/" element={
+//                 <div>
+//                     <SearchBar handleSearch = {handleSearch}/>
+//                     <Gallery data={data} />
+//                 </div>
+//             }/>
+//             <DataContext.Provider value={data}>
+//               <Gallery />
+//               <Route path="./album/:id" element={<AlbumView/>} />
+//               <Route path="./artist/:id" element={<ArtistView/>} />
+//             </DataContext.Provider>
+//           </Routes>
+//         </Router>
+//         <DataContext />
+//       </div>
+//     );
+// }
+
+// export default App;
+
+
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,13 +1,59 @@
 import { useState } from 'react'
+import {Link} from 'react-router-dom'
 
-function GalleryItem(props){
-    let [view, setView] = useState(false)
-
+const detailView = () => {
     return (
-        <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
-            <p>{props.item.trackCensoredName}</p>
+        <div style={detailStyle}>
+            <h2>{props.item.trackName}</h2>
+            <h3>
+            <Link to={`/artist/${props.item.artistId}`}>
+                         {props.item.artistName}
+                     </Link>
+                 </h3>
+                 <h3>
+                     <Link to={`/album/${props.item.collectionId}`}>
+                         {props.item.collectionName}
+                     </Link>
+            </h3>
+            <h4>{props.item.primaryGenreName}</h4>
+            <h4>{props.item.releaseDate}</h4>
         </div>
     )
 }
 
+
 export default GalleryItem
+
+
+
+
+// import { useState } from 'react'
+// import {Link} from 'react-router-dom'
+
+// function GalleryItem(props){
+//    let [view, setView ] = useState(false)
+//         return (
+//             // <div style={detailStyle}>
+//             <div>
+//                 <h2>{props.item.trackName}</h2>
+//                 <h3>
+//                     <Link to={`/artist/${props.item.artistId}`}>
+//                         {props.item.artistName}
+//                     </Link>
+//                 </h3>
+//                 <h3>
+//                     <Link to={`/album/${props.item.collectionId}`}>
+//                         {props.item.collectionName}
+//                     </Link>
+//                 </h3>
+//                 <h4>{props.item.primaryGenreName}</h4>
+//                 <h4>{props.item.releaseDate}</h4>
+//             </div>
+//         )
+//     }
+    
+
+// export default GalleryItem
+
+
+// <img src={props.item.artworkUrl100} alt={'test'}></img>
